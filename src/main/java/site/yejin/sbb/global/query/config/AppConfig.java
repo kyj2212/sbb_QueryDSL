043e5397b71082e349c82d8a4e0200f8ab1e6df7
kyj2212 <kyj2212@gmail.com>
2022-08-26 13:34:01 +0900

    Feat : add jpa configuration

--- src/main/java/site/yejin/sbb/global/query/config/AppConfig.java
+++ src/main/java/site/yejin/sbb/global/query/config/AppConfig.java
@@ -1,2 +1,17 @@
-package site.yejin.sbb.global.query.config;public class AppConfig {
+package site.yejin.sbb.global.query.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+@Slf4j
+public class AppConfig {
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
 }
