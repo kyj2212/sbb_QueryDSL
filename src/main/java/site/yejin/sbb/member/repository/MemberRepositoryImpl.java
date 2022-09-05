279c37753938872394fa898e0a3e2ddf1236919d
kyj2212 <kyj2212@gmail.com>
2022-09-06 04:16:23 +0900

    Fix : add 3case get interest content, by following

--- src/main/java/site/yejin/sbb/member/repository/MemberRepositoryImpl.java
+++ src/main/java/site/yejin/sbb/member/repository/MemberRepositoryImpl.java
@@ -4,16 +4,16 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import site.yejin.sbb.member.entity.Member;
-import site.yejin.sbb.interestkeyword.InterestKeyword;
+import site.yejin.sbb.member.entity.QMember;
 
 import java.util.List;
 
-import static site.yejin.sbb.member.entity.QMember.member;
 import static site.yejin.sbb.interestkeyword.QInterestKeyword.interestKeyword;
+import static site.yejin.sbb.member.entity.QMember.member;
 
 @Repository
 @RequiredArgsConstructor
-public class MemberRepositoryImpl implements MemberRepositoryCustom{
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
 
@@ -27,7 +27,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
     }
 
     @Override
-    public List<String> getQslInterestKeywordsByFollowingsOf(Member m){
+    public List<String> getQslInterestKeywordsByFollowingsOf(Member m) {
         return jpaQueryFactory.select(interestKeyword.content)
                 .from(member)
                 .innerJoin(interestKeyword)
@@ -35,13 +35,51 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 .where(member.eq(m))
                 .fetch();
     }
+
+        @Override
+        public List<String> getQslDistinctInterestKeywordsByFollowingsOf(Member m){
+            return jpaQueryFactory.selectDistinct(interestKeyword.content)
+                    .from(member)
+                    .innerJoin(interestKeyword)
+                    .on(member.followings.any().eq(interestKeyword.member))
+                    .where(member.eq(m))
+                    .fetch();
+        }
+/*
     @Override
     public List<String> getQslDistinctInterestKeywordsByFollowingsOf(Member m){
+        QMember member2 = new QMember("member2");
         return jpaQueryFactory.selectDistinct(interestKeyword.content)
-                .from(member)
-                .innerJoin(interestKeyword)
-                .on(member.followings.any().eq(interestKeyword.member))
-                .where(member.eq(m))
+                .from(interestKeyword)
+                .innerJoin(interestKeyword.member, member) // site_user
+                .innerJoin(member.followers, member2)
+                .where(member2.id.eq(m.getId()))
                 .fetch();
     }
+*/
+/*    @Override
+    public List<String> getQslDistinctInterestKeywordsByFollowingsOf(Member m) {
+
+        return jpaQueryFactory.select(interestKeyword.content).distinct()
+                .from(interestKeyword)
+                .where(interestKeyword.member.in(m.getFollowings()))
+                .fetch();
+
+    }*/
+    /*
+    @Override
+    public List<String> getQslDistinctInterestKeywordsByFollowingsOf(Member m) {
+        QMember member2 = new QMember("M2");
+
+        List<Long> ids = jpaQueryFactory.select(member.id)
+                .from(member)
+                .innerJoin(member.followers, member2)
+                .where(member2.id.eq(m.getId()))
+                .fetch();
+
+        return jpaQueryFactory.select(interestKeyword.content).distinct()
+                .from(interestKeyword)
+                .where(interestKeyword.member.id.in(ids))
+                .fetch();
+    }*/
 }
